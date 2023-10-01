7b7d3a25debb39cc1abbdfd2d2f6788f8e578cad
mino <mino4084@gmail.com>
2023-10-01 15:52:06 +0900

    get HTTP 요청을 받으면 HTTP 프로토콜 내용을 을 BufferedReader로 로그 확인

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -3,9 +3,10 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
     private final int port;
@@ -26,6 +27,33 @@ public class CustomWebApplicationServer {
             // 서버 소켓을 만든 다음, accept로 클라이언트를 기다린다
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected");
+
+                /**
+                 * step 1 : 사용자 요청을 메인 Thread가 처리
+                 */
+
+                try (
+                        InputStream in = clientSocket.getInputStream();
+                        OutputStream out = clientSocket.getOutputStream();
+                ) {
+                    BufferedReader br =
+                            new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+                    String line;
+
+                    while ((line = br.readLine()) != ""){
+                        System.out.println(line);
+                        /**
+                         * 아래 내용대로 로그 출력
+                         * GET / HTTP/1.1
+                         * Host: localhost:8080
+                         * Connection: Keep-Alive
+                         * User-Agent: Apache-HttpClient/4.5.13 (Java/17.0.4)
+                         * Accept-Encoding: gzip,deflate
+                         */
+                    }
+
+                }
             }
         }
     }
