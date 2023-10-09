88fbbb1b5e6811c0bcc473507b6a41fe6a460175
mino <mino4084@gmail.com>
2023-10-09 14:20:09 +0900

    HttpResponse 객체 생성

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +34,8 @@ public class CustomWebApplicationServer {
                  * step 1 : 사용자 요청을 메인 Thread가 처리
                  */
 
+
+
                 try (
                         InputStream in = clientSocket.getInputStream();
                         OutputStream out = clientSocket.getOutputStream();
@@ -39,18 +43,41 @@ public class CustomWebApplicationServer {
                     BufferedReader br =
                             new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
                     DataOutputStream dos = new DataOutputStream(out);
-                    String line;
 
+
+                    /**
+                     * 아래 내용대로 로그 출력
+                     * GET / HTTP/1.1
+                     * Host: localhost:8080
+                     * Connection: Keep-Alive
+                     * User-Agent: Apache-HttpClient/4.5.13 (Java/17.0.4)
+                     * Accept-Encoding: gzip,deflate
+                     */
+
+                    /*
+
+                    String line;
                     while ((line = br.readLine()) != ""){
                         System.out.println(line);
-                        /**
-                         * 아래 내용대로 로그 출력
-                         * GET / HTTP/1.1
-                         * Host: localhost:8080
-                         * Connection: Keep-Alive
-                         * User-Agent: Apache-HttpClient/4.5.13 (Java/17.0.4)
-                         * Accept-Encoding: gzip,deflate
-                         */
+
+                    }
+                     */
+
+                    HttpRequest httpRequest = new HttpRequest(br);
+
+                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
+                         QueryStrings queryStrings = httpRequest.getQueryStrings();
+
+                         int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+                         String operator = queryStrings.getValue("operator");
+                         int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+
+                         int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                         byte[] body = String.valueOf(result).getBytes();
+
+                         HttpResponse response = new HttpResponse(dos);
+                         response.response200Header("application/json", body.length);
+                         response.responseBody(body);
                     }
 
                 }
